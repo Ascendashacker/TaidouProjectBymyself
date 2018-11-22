f66f70dd50b67885384b7bfce3cbd4ef9a21e95f
Ascendashacker <327281966@qq.com>
2018-11-22 23:41:42 +0800

    新增readme项目说明

---
+++ ToolBag/Assets/Script/1.LoadImage/LoadImage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadImage : MonoBehaviour {
+    /*该脚本介绍Unity加载贴图的几种方式*/
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ToolBag/README.md
@@ -0,0 +1,5 @@
+# ToolBag
+## 1.Unity加载图片的几种方式
+### （1）WWW方式
+### （2）Resouce.load()方式
+### （3）File方式
