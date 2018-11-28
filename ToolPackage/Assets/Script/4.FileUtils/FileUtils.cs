c7963ec7c5ec9088b13b7a1c3500aa6e4e64636a
Ascnedashacker <327281966@qq.com>
2018-11-28 17:09:45 +0800

    单例基类

---
+++ ToolPackage/Assets/Script/4.FileUtils/FileUtils.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.IO;
+
+public class FileUtils: Singleton<FileUtils> {
+    /// <summary>
+    /// 将文件转换成byte[] 数组
+    /// </summary>
+    /// <param name="fileUrl">文件路径文件名称</param>
+    /// <returns>byte[]</returns>
+    public byte[] GetFileByteData(string fileUrl)
+    {
+        FileStream fs = new FileStream(fileUrl, FileMode.Open, FileAccess.Read);
+        try
+        {
+            byte[] buffur = new byte[fs.Length];
+            fs.Read(buffur, 0, (int)fs.Length);
+            return buffur;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+        finally
+        {
+            if (fs != null)
+            {
+                //关闭资源
+                fs.Close();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 将文件转换成byte[] 数组
+    /// </summary>
+    /// <param name="fileUrl">文件路径文件名称</param>
+    /// <returns>byte[]</returns>
+    public byte[] AuthGetFileData(string fileUrl)
+    {
+        using (FileStream fs = new FileStream(fileUrl, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        {
+            byte[] buffur = new byte[fs.Length];
+            using (BinaryWriter bw = new BinaryWriter(fs))
+            {
+                bw.Write(buffur);
+                bw.Close();
+            }
+            return buffur;
+        }
+    }
+}
+
---
+++ ToolPackage/Assets/Script/5.Singleton/Singleton.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 单例基类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Singleton<T> where T : new()
+{
+    public static T Instance
+    {
+        get { return SingletonCreator.instance; }
+    }
+    class SingletonCreator
+    {
+        internal static readonly T instance = new T();
+    }
+}
